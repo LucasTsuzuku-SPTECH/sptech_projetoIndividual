741014b6aa1b6adf0b9b826194d9787b2bfc3ce1
LucasTsuzuku-SPTECH <lucas.tsuzuku@sptech.school>
2025-05-14 23:55:57 -0300

    ajustes cadastro outer worlds

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -56,24 +56,25 @@ function cadastrar(req, res) {
   var nome = req.body.nomeServer;
   var email = req.body.emailServer;
   var senha = req.body.senhaServer;
-  var fkEmpresa = req.body.idEmpresaVincularServer;
-  var cpf = req.body.cpfServer;
+  var telefone = req.body.senhaServer;
+  var pais = req.body.senhaServer;
+  var senha = req.body.senhaServer;
 
   // Faça as validações dos valores
   if (nome == undefined) {
     res.status(400).send("Seu nome está undefined!");
   } else if (email == undefined) {
     res.status(400).send("Seu email está undefined!");
-  } else if (cpf == undefined) {
-    res.status(400).send("Seu cpf está undefined!");
+  } else if (telefone == undefined) {
+    res.status(400).send("Seu telefone está undefined!");
   } else if (senha == undefined) {
     res.status(400).send("Sua senha está undefined!");
-  } else if (fkEmpresa == undefined) {
-    res.status(400).send("Sua empresa a vincular está undefined!");
+  } else if (pais == undefined) {
+    res.status(400).send("Seu país|grupo está undefined!");
   } else {
     // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
     usuarioModel
-      .cadastrar(nome, email, senha, fkEmpresa, cpf)
+      .cadastrar(nome, email, telefone, senha, pais)
       .then(function (resultado) {
         res.json(resultado);
       })
