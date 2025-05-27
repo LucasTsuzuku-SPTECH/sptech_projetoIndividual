e43e41fd72716906dff9516d634eac82f27a0029
LucasTsuzuku-SPTECH <lucas.tsuzuku@sptech.school>
2025-05-27 20:53:43 -0300

    atualizacao valores habilidades

--- public/js/personagemCompanheiros.js
+++ public/js/personagemCompanheiros.js
@@ -18,13 +18,45 @@ function adicionarCompanheiro(idCompanheiro) {
     ) {
       quantidadeCompanheirosDisponiveis = quantidadeCompanheirosDisponiveis - 1;
       console.log(quantidadeCompanheirosDisponiveis);
+      if (checkbox[i].value == "Parvati") {
+        habilidadePersonagem.habilidadePersuasao += 19;
+        habilidadePersonagem.habilidadeGazua += 21;
+        habilidadePersonagem.habilidadeEngenharia += 22;
+        console.log(habilidadePersonagem);
+      } else if (checkbox[i].value == "Max") {
+        habilidadePersonagem.habilidadeIntimidacao += 19;
+        habilidadePersonagem.habilidadeHacking += 20;
+        habilidadePersonagem.habilidadeCiencia += 22;
+        console.log(habilidadePersonagem);
+      } else if (checkbox[i].value == "Ellie") {
+        habilidadePersonagem.habilidadeMentira += 18;
+        habilidadePersonagem.habilidadeMedicina += 22;
+        habilidadePersonagem.habilidadeEngenharia += 19;
+        console.log(habilidadePersonagem);
+      } else if (checkbox[i].value == "Nyoka") {
+        habilidadePersonagem.habilidadeMentira += 18;
+        habilidadePersonagem.abilidadeMedicina += 19;
+        habilidadePersonagem.habilidadeFurtividade += 21;
+        console.log(habilidadePersonagem);
+      } else if (checkbox[i].value == "Felix") {
+        habilidadePersonagem.habilidadePersuasao += 20;
+        habilidadePersonagem.habilidadeGazua += 20;
+        habilidadePersonagem.habilidadeFurtividade += 19;
+        console.log(habilidadePersonagem);
+      } else if (checkbox[i].value == "SAM") {
+        habilidadePersonagem.habilidadeEngenharia += 19;
+        habilidadePersonagem.habilidadeHacking += 20;
+        habilidadePersonagem.habilidadeCiencia += 20;
+        console.log(habilidadePersonagem);
+        break;
+      }
       arrayCompanheirosSelecionados.push(checkbox[i].value);
       console.log(arrayCompanheirosSelecionados);
     } else if (checkbox[i].checked == false) {
       console.log(quantidadeCompanheirosDisponiveis);
     }
-    atualizarPontosDisponiveis();
   }
+  atualizarPontosDisponiveis();
 }
 
 function resetarCompanheiros() {
