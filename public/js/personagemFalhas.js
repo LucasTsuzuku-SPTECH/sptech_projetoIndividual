17ec1a708181bf14f1efeb721199ffa155429056
LucasTsuzuku-SPTECH <lucas.tsuzuku@sptech.school>
2025-05-27 15:03:47 -0300

    falha completo

---
+++ public/js/personagemFalhas.js
@@ -0,0 +1,79 @@
+function alterarDescricaoFalha(number) {
+  if (number == 1) {
+    cardInfoFalhas.innerHTML = `
+      <h3>Acrofobia</h3>
+                  <p>
+                    Depois de cair repetidamente de grandes alturas, você não funciona mais bem quando o chão está muito abaixo de você.
+                  </p>
+                  
+      `;
+  } else if (number == 2) {
+    cardInfoFalhas.innerHTML = `
+      <h3>Cinofobia</h3>
+                  <p>
+                    Atacado muitas vezes por canídeos, você fica nervoso sempre que um deles ataca, o que o torna menos eficaz.
+                  </p>
+                  
+      `;
+  } else if (number == 3) {
+    cardInfoFalhas.innerHTML = `
+      <h3>Dependência de Drogas</h3>
+            <p>
+              Tomar tantas drogas quanto você já fez se tornar um vício, fazendo com que você sofra abstinência se ficar muito tempo sem outra dose.
+            </p>
+            
+          `;
+  } else if (number == 4) {
+    cardInfoFalhas.innerHTML = `
+      <h3>Paranóico</h3>
+            <p>
+              Ser pego repetidamente por guardas em Áreas Restritas deixa você nervoso, ainda mais quando você está vagando por Áreas Restritas ou se esgueirando.
+            </p>
+            
+        `;
+  } else if (number == 5) {
+    cardInfoFalhas.innerHTML = `
+      <h3>Robofobia</h3>
+            <p>
+              Os robôs agora lhe dão arrepios, e quando eles atacam seu grupo, você não é tão eficaz quanto deveria.
+            </p>
+            
+`;
+  } else if (number == 6) {
+    cardInfoFalhas.innerHTML = `
+      <h3>Fraqueza por Dano Físico</h3>
+            <p>
+              Receber muito dano físico deixou você vulnerável e vulnerável a mais dano físico.
+            </p>
+            
+    `;
+  } else if (number == 7) {
+    cardInfoFalhas.innerHTML = `
+      <h3>Nenhuma Falha</h3>
+            <p>
+              Nenhuma falha selecionada
+            </p>
+            
+    `;
+  }
+}
+
+function confirmarFalha() {
+  const radios = document.getElementsByName("radFalha");
+  console.log(radios);
+  falhaSelecionado = null;
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      falhaSelecionado = radios[i].value;
+      avisoAptidao.innerHTML = `
+        Falha selecionado: ${falhaSelecionado}
+        `;
+      break; // Para após encontrar o primeiro radio selecionado
+    }
+  }
+  if (falhaSelecionado == "") {
+    falhaSelecionado = null;
+  }
+
+  console.log("Valor selecionado:", falhaSelecionado); // Imprime o valor no console
+}
