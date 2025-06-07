d5f4158e8847dbb0ca5ea291aea2fecb5359cbc9
LucasTsuzuku-SPTECH <lucas.tsuzuku@sptech.school>
2025-06-07 20:07:29 -0300

    criacao teste 3

--- public/js/testes.js
+++ public/js/testes.js
@@ -36,7 +36,6 @@ function validarEscolhaTeste1(idRadioSelecionado) {
 }
 
 function validarEscolhaTeste2(idRadioSelecionado) {
-  console.log(!arrayCompanheirosSelecionados.includes("Nyoka"));
   const radios = document.getElementsByName("radTeste2");
   const radioClicado = document.getElementById(idRadioSelecionado);
   if (
@@ -44,7 +43,7 @@ function validarEscolhaTeste2(idRadioSelecionado) {
     !arrayCompanheirosSelecionados.includes("Nyoka") &&
     !arrayCompanheirosSelecionados.includes("Ellie")
   ) {
-    alert("Você não o companheiro requisitado, escolha outra opção");
+    alert("Você não tem o companheiro requisitado, escolha outra opção");
     radioClicado.checked = false;
     console.log("Escolha selecionado: ", escolhaTeste2); // Imprime o valor no console
     return;
@@ -75,4 +74,41 @@ function validarEscolhaTeste2(idRadioSelecionado) {
   }
 
   console.log("Escolha selecionado: ", escolhaTeste2); // Imprime o valor no console
+}
+
+function validarEscolhaTeste3(idRadioSelecionado) {
+  const radios = document.getElementsByName("radTeste3");
+  const radioClicado = document.getElementById(idRadioSelecionado);
+  if (radios[0].checked && !arrayCompanheirosSelecionados.includes("Parvati")) {
+    alert("Você não tem o companheiro requisitado, escolha outra opção");
+    radioClicado.checked = false;
+    console.log("Escolha selecionado: ", escolhaTeste3); // Imprime o valor no console
+    return;
+  }
+  if (radios[1].checked && !arrayCompanheirosSelecionados.includes("Max")) {
+    alert("Você não tem companheiro requisitado, escolha outra opção");
+    radioClicado.checked = false;
+    console.log("Escolha selecionado: ", escolhaTeste3); // Imprime o valor no console
+    return;
+  }
+  if (
+    radios[2].checked &&
+    habilidadePersonagem.habilidadeEngenharia < 30 &&
+    habilidadePersonagem.habilidadeHacking < 30
+  ) {
+    alert("Você não tem pontos o suficiente, escolha outra opção");
+    radioClicado.checked = false;
+    console.log("Escolha selecionado: ", escolhaTeste3); // Imprime o valor no console
+    return;
+  }
+
+  console.log(radios);
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      escolhaTeste3 = radios[i].value;
+      break; // Para após encontrar o primeiro radio selecionado
+    }
+  }
+
+  console.log("Escolha selecionado: ", escolhaTeste3); // Imprime o valor no console
 }
