faa2ecd83fa202346efd29cb9cb200af7c2275b3
LucasTsuzuku-SPTECH <lucas.tsuzuku@sptech.school>
2025-06-07 19:45:28 -0300

    teste2 adicionado

--- public/js/testes.js
+++ public/js/testes.js
@@ -34,3 +34,45 @@ function validarEscolhaTeste1(idRadioSelecionado) {
 
   console.log("Escolha selecionado: ", escolhaTeste1); // Imprime o valor no console
 }
+
+function validarEscolhaTeste2(idRadioSelecionado) {
+  console.log(!arrayCompanheirosSelecionados.includes("Nyoka"));
+  const radios = document.getElementsByName("radTeste2");
+  const radioClicado = document.getElementById(idRadioSelecionado);
+  if (
+    radios[0].checked &&
+    !arrayCompanheirosSelecionados.includes("Nyoka") &&
+    !arrayCompanheirosSelecionados.includes("Ellie")
+  ) {
+    alert("Você não o companheiro requisitado, escolha outra opção");
+    radioClicado.checked = false;
+    console.log("Escolha selecionado: ", escolhaTeste2); // Imprime o valor no console
+    return;
+  }
+  if (
+    radios[1].checked &&
+    (habilidadePersonagem.habilidadeArmasPesadas < 30 ||
+      habilidadePersonagem.habilidadeIntimidacao < 20)
+  ) {
+    alert("Você não tem pontos o suficiente, escolha outra opção");
+    radioClicado.checked = false;
+    console.log("Escolha selecionado: ", escolhaTeste2); // Imprime o valor no console
+    return;
+  }
+  if (radios[2].checked && habilidadePersonagem.habilidadeMedicina < 30) {
+    alert("Você não tem pontos o suficiente, escolha outra opção");
+    radioClicado.checked = false;
+    console.log("Escolha selecionado: ", escolhaTeste2); // Imprime o valor no console
+    return;
+  }
+
+  console.log(radios);
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      escolhaTeste2 = radios[i].value;
+      break; // Para após encontrar o primeiro radio selecionado
+    }
+  }
+
+  console.log("Escolha selecionado: ", escolhaTeste2); // Imprime o valor no console
+}
