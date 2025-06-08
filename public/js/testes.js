a09a49aa6cd084c3ba204019759a12baa84571c4
LucasTsuzuku-SPTECH <lucas.tsuzuku@sptech.school>
2025-06-07 22:48:57 -0300

    teste criado com sucesso

--- public/js/testes.js
+++ public/js/testes.js
@@ -129,7 +129,7 @@ function mostrarResultadoTeste1() {
             >
               <p>
                 Bem, pelo menos podemos dizer que você morreu lutando de forma
-                heróica (ou estúpida, dependendo do caso)
+                heróica (ou estúpida, dependendo do caso).
               </p>
             </div>
     `;
@@ -148,6 +148,140 @@ function mostrarResultadoTeste1() {
             >
               <p>
                 Parabéns ! A peça foi recuperada e você ainda pode aproveitar a Vanguarda a medida que a temperatura diminui e a tripulação te saúda como grande salvador!
+              </p>
+            </div>
+    `;
+  } else {
+    tituloResultadoTeste_valorResultadoTeste2.innerHTML = "INDEFINIDO";
+    dash__div_cards_informacoes__resultadoTeste2.innerHTML = `
+    <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultado"
+            >
+              <img src="../assets/imgs/logoSpacersChoice.png alt="Erro" />
+            </div>
+            <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultadoDescricao"
+            >
+              <p>
+                Erro: Resultado deu indefinido, reinicie a página.
+              </p>
+            </div>
+    `;
+  }
+}
+
+function mostrarResultadoTeste2() {
+  if (escolhaTeste2 == 0) {
+    tituloResultadoTeste_valorResultadoTeste2.innerHTML = "REPROVADO";
+    dash__div_cards_informacoes__resultadoTeste2.innerHTML = `
+    <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultado"
+            >
+              <img src="../assets/imgs/resultado2Teste2.jpg" alt="" />
+            </div>
+            <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultadoDescricao"
+            >
+              <p>
+                Os Louvassauros não pareciam estarem abertos para a um novo estilo de vida, mas valorizo sua tentativa.
+              </p>
+            </div>
+    `;
+  } else if (escolhaTeste2 == 1) {
+    tituloResultadoTeste_valorResultadoTeste2.innerHTML = "APROVADO";
+    dash__div_cards_informacoes__resultadoTeste2.innerHTML = `
+    <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultado"
+            >
+              <img src="../assets/imgs/resultadoTeste2.jpg" alt="" />
+            </div>
+            <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultadoDescricao"
+            >
+              <p>
+                Parabéns ! A sala está sem presença de animais e você conseguiu seguir adiante na missão !
+              </p>
+            </div>
+    `;
+  } else {
+    tituloResultadoTeste_valorResultadoTeste2.innerHTML = "INDEFINIDO";
+    dash__div_cards_informacoes__resultadoTeste2.innerHTML = `
+    <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultado"
+            >
+              <img src="../assets/imgs/logoSpacersChoice.png alt="Erro" />
+            </div>
+            <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultadoDescricao"
+            >
+              <p>
+                Erro: Resultado deu indefinido, reinicie a página.
+              </p>
+            </div>
+    `;
+  }
+}
+
+function mostrarResultadoTeste3() {
+  if (escolhaTeste3 == 0) {
+    tituloResultadoTeste_valorResultadoTeste3.innerHTML = "REPROVADO";
+    dash__div_cards_informacoes__resultadoTeste3.innerHTML = `
+    <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultado"
+            >
+              <img src="../assets/imgs/resultado2Teste3.jpg" alt="" />
+            </div>
+            <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultadoDescricao"
+            >
+              <p>
+               A nave explodiu e você vai ter que ficar no planeta por mais alguns anos (sem contar que você vai ter que preencher as vagas de tripulantes que acabaram de abrir).
+              </p>
+            </div>
+    `;
+  } else if (escolhaTeste3 == 1) {
+    tituloResultadoTeste_valorResultadoTeste3.innerHTML = "APROVADO";
+    dash__div_cards_informacoes__resultadoTeste3.innerHTML = `
+    <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultado"
+            >
+              <img src="../assets/imgs/resultadoTeste3.jpg" alt="" />
+            </div>
+            <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultadoDescricao"
+            >
+              <p>
+                Parabéns ! O gás foi transferido para sua nave, agora é só a entrega e a tripulação está garantida por meses !
+              </p>
+            </div>
+    `;
+  } else {
+    tituloResultadoTeste_valorResultadoTeste2.innerHTML = "INDEFINIDO";
+    dash__div_cards_informacoes__resultadoTeste2.innerHTML = `
+    <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultado"
+            >
+              <img src="../assets/imgs/logoSpacersChoice.png alt="Erro" />
+            </div>
+            <div
+              class="dash__div_cards_informacoes__card"
+              id="dash__div_cards_informacoes__card__resultadoDescricao"
+            >
+              <p>
+                Erro: Resultado deu indefinido, reinicie a página.
               </p>
             </div>
     `;
